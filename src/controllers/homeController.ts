eb6dc6da28fa9c67ec8e0eba752f5da98fb057e5
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-11-12 17:02:45 -0300

    Projeto

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,8 +3,24 @@ import {sequelize} from '../instances/mysql'
 import {User} from '../models/User'
 
 export const home = async (req: Request, res: Response)=>{
-
     res.render('pages/home', {
-        
+         
     });
+    
+    let nome: string
+    let data: number
+    let endereco: string
+    let bairro: string
+    let cidade: string
+    let estado: string
+    let cep: number
+    let tel_res: number
+    let tel: number
+    let tel_emergencia:number
+    let contato: number
+    let data_nasc: number
+    let profissao: string
+    let email: string
+    let sexo: string
 };
+
